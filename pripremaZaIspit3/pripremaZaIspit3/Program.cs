eae50555b8f9e584cbbd5cb6013cb4d6c2aa32c6
Evian-dev <evian.rebic@gmail.com>
2024-12-09 13:29:34 +0100

    Add files via upload

---
+++ pripremaZaIspit3/pripremaZaIspit3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaIspit3
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        { 
+        
+        }
+
+        
+            class KlasaX
+        {
+            private int broj = 10;
+            public int Broj
+            {
+                get { return broj; }
+                set { broj = value; }
+            }
+        }
+
+       
+
+        }
+}
+
